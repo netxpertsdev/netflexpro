8f87b0c79bd62d9aea8638fa9664983ec2c67e90
ajith <ajith@gmail.com>
2024-11-17 12:57:05 +0000

    Conflict Resolved 5th Code

--- Sales.java
+++ Sales.java
@@ -5,3 +5,8 @@ public class Main {
 }
 int myNum = 15;
 System.out.println(myNum);
+int myNum = 15;
+myNum = 20;  // myNum is now 20
+System.out.println(myNum);
+final int myNum = 15;
+myNum = 20;  // will generate an error: cannot assign a value to a final variable
